bba943b4b27fa7125fa6f4d7af18096c3016f4a7
Alexandr Struchkov <alexpodoff@gmail.com>
2016-12-10 16:05:40 +0300

    modify from file.json

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,12 +3,13 @@ from model.group import Group
 from random import randrange
 
 
-def test_modify_group_name(app):
+def test_modify_group_name(app, json_groups):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
-    group = Group(name="new name")
+    group = json_groups
+    # group = Group(name="new name")
     group.id = old_groups[index].id
     app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
