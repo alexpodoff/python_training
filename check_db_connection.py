da138e1edc4958255050cfd8fd14de7681f685f5
Alexandr Struchkov <alexpodoff@gmail.com>
2016-12-16 19:03:16 +0300

    something goes wrong...

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,16 +1,17 @@
-from fixture.db import DbFixture
-# import pymysql.cursors
+from fixture.orm import ORMFixture
+from model.group import Group
 
-
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    somelist = db.get_contacts_in_group(Group(id='50'))
+    for item in somelist:
+        print(item)
+    print(len(somelist))
 finally:
-    db.destroy()
+    pass #db.destroy()
+
+
 
 # connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 #
--- model/contact.py
+++ model/contact.py
@@ -31,7 +31,6 @@ class Contact:
     def __repr__(self):
         return '%s:"%s":"%s":%s:%s:%s' % (self.id, self.name, self.lastname, self.email, self.email2, self.email3)
 
-
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
 
