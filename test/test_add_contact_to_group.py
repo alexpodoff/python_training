5c3e3a5dd51eb53147468a7856d9b75b6e80dbd6
Alexandr Struchkov <alexpodoff@gmail.com>
2017-01-10 22:49:07 +0300

    minor fix

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -15,17 +15,11 @@ def test_add_contact_to_group(app, db):
         app.group.create(Group(name="test"))
     contacts = app.contact.get_contact_list()
     contact = random.choice(contacts)
-    index = -1
-    for i in contacts:
-        index = index + 1
-        if i.id == contact.id:
-            break
     groups = db.get_group_list()
     group = random.choice(groups)
     condition = base.get_contacts_in_group(Group(id=group.id))
     if contact in condition:
         app.contact.remove_contact_from_group(contact.id, group.id)
-    print(index)
     print("group name = " + group.name)
     print("contact = " + contact.name)
     app.contact.add_contact_to_group(contact.id, group.id)
--- test/test_del_from_group.py
+++ test/test_del_from_group.py
@@ -15,11 +15,6 @@ def test_remove_contact_from_group(app, db):
         app.group.create(Group(name="test"))
     contacts = app.contact.get_contact_list()
     contact = random.choice(contacts)
-    index = -1
-    for i in contacts:
-        index = index + 1
-        if i.id == contact.id:
-            break
     groups = db.get_group_list()
     group = random.choice(groups)
     condition = base.get_contacts_in_group(Group(id=group.id))
